5f3afdf8b79312dbfdec3d7e321ba3bc9b0b5040
potatoken777 <122539617+potatoken777@users.noreply.github.com>
2023-01-31 10:19:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,4 @@
-input.onGesture(Gesture.Shake, function () {
-    reset()
-})
-function reset () {
-    OLED.init(128, 64)
-    p1 = 0
-    p2 = 0
-    ties = 0
-    rounds = 0
-    OLED.writeStringNewLine("shall we play a game")
-    basic.pause(2000)
+function function20 () {
     OLED.clear()
     OLED.writeStringNewLine("Player1score:" + p1)
     OLED.newLine()
@@ -17,9 +7,46 @@ function reset () {
     OLED.writeStringNewLine("ties:" + ties)
     OLED.newLine()
     OLED.writeStringNewLine("rounds:" + rounds)
+    function20()
 }
+input.onButtonPressed(Button.A, function () {
+    OLED.writeStringNewLine("Player1score:" + p1)
+    basic.showString("A")
+    counter1 += 1
+    basic.showNumber(counter1)
+    OLED.writeStringNewLine("rounds:" + rounds)
+})
+function function10 () {
+    OLED.init(128, 64)
+    p1 = 0
+    p2 = 0
+    ties = 0
+    rounds = 0
+    OLED.writeStringNewLine("play now!!!!!!")
+    basic.pause(2000)
+}
+input.onButtonPressed(Button.AB, function () {
+    OLED.writeStringNewLine("ties:" + ties)
+    basic.showString("T")
+    counter3 += 1
+    basic.showNumber(counter3)
+    OLED.writeStringNewLine("rounds:" + rounds)
+})
+input.onButtonPressed(Button.B, function () {
+    OLED.writeStringNewLine("Player2score:" + p2)
+    basic.showString("B")
+    counter2 += 1
+    basic.showNumber(counter2)
+    OLED.writeStringNewLine("rounds:" + rounds)
+})
+input.onGesture(Gesture.Shake, function () {
+    function10()
+})
+let counter2 = 0
+let counter3 = 0
+let counter1 = 0
 let rounds = 0
 let ties = 0
 let p2 = 0
 let p1 = 0
-reset()
+function10()
